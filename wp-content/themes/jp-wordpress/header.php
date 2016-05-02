1d22dd9b4caf4c0af6fd0309363a0e4fdcf3b212
Bancroy <bancroy@gmail.com>
2016-05-02 18:13:59 +0200

    Branding

--- wp-content/themes/jp-wordpress/header.php
+++ wp-content/themes/jp-wordpress/header.php
@@ -3,14 +3,15 @@
 	<head>
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta charset="<?php bloginfo('charset'); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="<?php bloginfo('description'); ?>">
 		<title><?php
             bloginfo('name');
             if(wp_title('', false))
                 echo ': ';
             wp_title('');
         ?></title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo('description'); ?>">
+        <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico" type="image/x-icon">
 		<?php wp_head(); ?>
 	</head>
 	<body>
